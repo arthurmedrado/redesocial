4fd6c1072da390321bfa7631731f0c05a6ffd982
Arthur MC <arthur.m.celedonio@gmail.com>
2017-02-01 10:28:28 -0200

    Atualização de comentarios

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::BaseController < ApplicationController
   protect_from_forgery with: :null_session
-  
+
   def survivors
     render json: {
       data: [
@@ -10,70 +10,101 @@ class Api::V0::BaseController < ApplicationController
       ]
     }
   end
-  
+
+  # cria sobreviventes na rede social
+  # :params:
+  #   name
+  #   genero
+  #   idade
+  #   lat
+  #   lon
+  #   agua
+  #   comida
+  #   medicamento
+  #   municao
+  # :return: true
   def criar_sobrevivente
-    @s = Sobrevivente.create(
+    @suvivor = Sobrevivente.create(
       name: params[:name], genero: params[:genero], idade: params[:idade],
       lat: params[:lat], lon: params[:lon],
       agua: params[:agua], comida: params[:comida], medicamento: params[:medicamento],
       municao: params[:municao]
     )
-    render json: @s
+    render json: @suvivor
   end
-  
-  
+
+
   # Efetua troca entre sobreviventes
   # :params:
   #   agua
   #   comida
-  # 
+  #   medicamento
+  #   municao
   # :return: true
   def troca
     @remetente = Sobrevivente.where(id: params[:de]).first
     @destinatario = Sobrevivente.where(id: params[:para]).first
-    
+
     enviar = {agua: 1, comida: 2, medicamento: 3, municao: 4}
     receber = {agua: 0, comida: 2, medicamento: 3, municao: 8}
-    
+
     trocou = @remetente.troca(@destinatario, enviar, receber)
-    
+
     render json: { status: trocou }
   end
-  
+
   # O metdoo faz isso
-  # rebe x, y, z, como parametros
+  # recebe x, y, z, como parametros
   # retorna a
   def reportar
     @sobrevivente = Sobrevivente.where(id: params[:de]).first
     @sobrevivente.reportar_contaminacao
   end
-  
+
+
   def relatorio
     @sobreviventes = Sobrevivente.all
-    
+
     contaminados = @sobreviventes.select{|s| s.contaminado? }.count
     nao_contaminados = @sobreviventes.select{|s| !s.contaminado? }.count
-    
+
     # @sobreviventes Lista com todos os sobreviventes
     # .select{|s| s.contaminado? } Selecionar só os que retornarem true
     # .map(&:value) tranformar cada objecto Sobrevivente no valor da função value
     # .inject(0, &:+) executa o metodo `+` entre os elementos do array, com 0 valor inicial
-    
+
     render json: {
       contaminados: 100 * contaminados / @sobreviventes.count,
       nao_contaminados: 100 * nao_contaminados / @sobreviventes.count,
       pontos_perdidos: @sobreviventes.select{|s| s.contaminado? }.map(&:value).inject(0, &:+)
     }
   end
-  
+
+
   def relatorio_contaminados
     @sobreviventes = Sobrevivente.all
-    
     contaminados = @sobreviventes.select{|s| s.contaminado? }.count
-    
+
     render json: {
       contaminados: 100 * contaminados / @sobreviventes.count
     }
   end
-  
+
+  def relatorio_nao_contaminados
+    @sobreviventes = Sobrevivente.all
+    nao_contaminados = @sobreviventes.select{|s| !s.contaminado? }.count
+
+    render json: {
+      nao_contaminados: 100 * nao_contaminados / @sobreviventes.count
+    }
+  end
+
+  def pontos_perdidos
+    @sobreviventes = Sobrevivente.all
+
+    render json: {
+      pontos_perdidos: @sobreviventes.select{|s| s.contaminado? }.map(&:value).inject(0, &:+)
+    }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+  # get -> Pegar uma lista (Ler)
+  # post -> Criar um objecto (Cria)
+  # put -> Atualizar um objeto que já existe (Atualiza)
+  # delete -> Deletar (Deleta)
   get '/api/v0/survivors', to: 'api/v0/base#survivors'
-  
-  # Parametros lidos: name, comida, agua, etc
+
+  # Parametros lidos: name, genero, idade, lat, lon, comida, agua, munição, medicamento
   post '/api/v0/novo_sobrevivente', to: 'api/v0/base#criar_sobrevivente'
-  
-  post '/api/v0/trocar/:de/:para', to: 'api/v0/base#troca'
+
   # /api/v0/trocar/1/2 => de 1 para 2
   # /api/v0/trocar/2/1 => de 2 para 1
-  
-  # get -> Pegar uma lista (consulta)
-  # post -> Criar um objecto
-  # put -> Atualizar um objeto que já existe
-  # delete -> Deletar
-  
+  post '/api/v0/trocar/:de/:para', to: 'api/v0/base#troca'
+
+  # Relatorio geral com os dados de contaminados, não contaminados e pontos perdidos
   get '/api/v0/relatorio', to: 'api/v0/base#relatorio'
+  # Relatorio de contaminados
   get '/api/v0/relatorio/contaminados', to: 'api/v0/base#relatorio_contaminados'
-  
-  
+  # Relatorio de não contaminados
+  get '/api/v0/relatorio/naocontaminados', to: 'api/v0/base#relatorio_nao_contaminados'
+  # Relatorio de pontos perdidos devido às infecções
+  get '/api/v0/relatorio/pontosperdidos', to: 'api/v0/base#pontos_perdidos'
+
+
+
 end
